f7762835617545b3b7d1acde7c8b6f69820b7418
Rudee007 <nikuswain01171432@gmail.com>
2026-03-22 19:50:55 +0530

    edited the main for env variable

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import { ThemeProvider } from './contexts/ThemeContext.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ThemeProvider } from './contexts/ThemeContext.tsx';
 
-const clientId = "602141817885-klelfc88erdleed8cenk0jktkbb9jcrb.apps.googleusercontent.com";
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+if (!clientId) {
+    console.error("Missing VITE_GOOGLE_CLIENT_ID! Make sure your .env file is set up correctly in the frontend directory.");
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId={clientId}>
+        <GoogleOAuthProvider clientId={clientId || ""}>
             <ThemeProvider>
                 <App />
             </ThemeProvider>
         </GoogleOAuthProvider>
     </React.StrictMode>,
-)
+);
